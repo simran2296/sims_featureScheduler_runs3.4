0812f62ae271efcaff0ea92903547afe2fbab9c6
Peter Yoachim <yoachim@uw.edu>
2023-12-04 12:02:13 -0800

    debugging

---
+++ initial/check_cross.py
@@ -0,0 +1,23 @@
+import numpy as np
+import sqlite3
+import pandas as pd
+from initial import sched_argparser, example_scheduler
+from rubin_scheduler.scheduler.utils import restore_scheduler
+from rubin_scheduler.scheduler.model_observatory import ModelObservatory
+
+
+if __name__ == "__main__":
+    args = sched_argparser()
+    args = args.parse_args([])
+    args.setup_only = True
+
+    scheduler = example_scheduler(args)
+
+    mo = ModelObservatory(mjd_start=60796.0)
+    sched, mo = restore_scheduler(188, scheduler, mo, 'm2_initial_v3.4_10yrs.db')
+    conditions = mo.return_conditions()
+
+    for sl in sched.survey_lists:
+        for survey in sl:
+            print(survey.survey_name, np.nanmax(survey.calc_reward_function(conditions)))
+
