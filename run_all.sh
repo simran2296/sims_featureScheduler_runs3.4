282e5e83c627ae17bef7577351270895c8f248cc
Peter Yoachim <yoachim@uw.edu>
2024-01-22 16:26:11 -0800

    slurms

---
+++ run_all.sh
@@ -0,0 +1,71 @@
+python mw/no_magell_roll.py --out_dir mw
+python noroll/noroll.py --out_dir noroll --mjd_plus 0
+python noroll/noroll.py --out_dir noroll --mjd_plus 60
+python noroll/noroll.py --out_dir noroll --mjd_plus 120
+python noroll/noroll.py --out_dir noroll --mjd_plus 180
+python noroll/noroll.py --out_dir noroll --mjd_plus 270
+python noroll/noroll.py --out_dir noroll --mjd_plus 365
+python roll_pause/roll_pause.py --out_dir roll_pause --mjd_plus 0
+python roll_pause/roll_pause.py --out_dir roll_pause --mjd_plus 60
+python roll_pause/roll_pause.py --out_dir roll_pause --mjd_plus 120
+python roll_pause/roll_pause.py --out_dir roll_pause --mjd_plus 180
+python roll_pause/roll_pause.py --out_dir roll_pause --mjd_plus 270
+python roll_pause/roll_pause.py --out_dir roll_pause --mjd_plus 365
+python too/too.py --out_dir too --nfollow 1 --too_rate 5
+python too/too.py --out_dir too --nfollow 1 --too_rate 10
+python too/too.py --out_dir too --nfollow 1 --too_rate 100
+python too/too.py --out_dir too --nfollow 4 --too_rate 5
+python too/too.py --out_dir too --nfollow 4 --too_rate 10
+python too/too.py --out_dir too --nfollow 4 --too_rate 100
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 27 --u_n_scale 0.9
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 27 --u_n_scale 1.0
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 27 --u_n_scale 1.1
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 27 --u_n_scale 1.2
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 27 --u_n_scale 1.5
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 30 --u_n_scale 0.9
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 30 --u_n_scale 1.0
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 30 --u_n_scale 1.1
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 30 --u_n_scale 1.2
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 30 --u_n_scale 1.5
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 38 --u_n_scale 0.9
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 38 --u_n_scale 1.0
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 38 --u_n_scale 1.1
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 38 --u_n_scale 1.2
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 38 --u_n_scale 1.5
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 45 --u_n_scale 0.9
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 45 --u_n_scale 1.0
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 45 --u_n_scale 1.1
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 45 --u_n_scale 1.2
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 45 --u_n_scale 1.5
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 60 --u_n_scale 0.9
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 60 --u_n_scale 1.0
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 60 --u_n_scale 1.1
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 60 --u_n_scale 1.2
+python vary_u_exp_n/internal_u.py --out_dir vary_u_exp_n --u_expt 60 --u_n_scale 1.5
+python weather/weather.py --out_dir weather --cloud_offset_year 0
+python weather/weather.py --out_dir weather --cloud_offset_year 1
+python weather/weather.py --out_dir weather --cloud_offset_year 2
+python weather/weather.py --out_dir weather --cloud_offset_year 4
+python weather/weather.py --out_dir weather --cloud_offset_year 6
+python weather/weather.py --out_dir weather --cloud_offset_year 8
+python weather/weather.py --out_dir weather --cloud_offset_year 10
+python weather/weather.py --out_dir weather --cloud_offset_year 12
+python weather/weather.py --out_dir weather --cloud_offset_year 14
+python weather/weather.py --out_dir weather --cloud_offset_year 16
+python weather/weather.py --out_dir weather --cloud_offset_year 18
+python weather/weather.py --out_dir weather --cloud_offset_year 20
+python weather/weather.py --out_dir weather --cloud_offset_year 30
+python weather/weather.py --out_dir weather --cloud_offset_year 31
+python weather/weather.py --out_dir weather --cloud_offset_year 35
+python weather/weather.py --out_dir weather --cloud_offset_year 36
+python ddf_loaded/ddf_loaded.py --out_dir ddf_loaded --ddf_flush_length 1
+python ddf_loaded/ddf_loaded.py --out_dir ddf_loaded --ddf_flush_length 2
+python ddf_loaded/ddf_loaded.py --out_dir ddf_loaded --ddf_flush_length 4
+python ddf_loaded/ddf_loaded.py --out_dir ddf_loaded --ddf_flush_length 8
+python ddf_loaded/ddf_loaded.py --out_dir ddf_loaded --ddf_flush_length 15
+python ddf_loaded_half/ddf_loaded_half.py --out_dir ddf_loaded_half --ddf_flush_length 1
+python ddf_loaded_half/ddf_loaded_half.py --out_dir ddf_loaded_half --ddf_flush_length 2
+python ddf_loaded_half/ddf_loaded_half.py --out_dir ddf_loaded_half --ddf_flush_length 4
+python ddf_loaded_half/ddf_loaded_half.py --out_dir ddf_loaded_half --ddf_flush_length 8
+python ddf_loaded_half/ddf_loaded_half.py --out_dir ddf_loaded_half --ddf_flush_length 15
+python ddf_loaded_half/ddf_half.py --out_dir ddf_loaded_half 
