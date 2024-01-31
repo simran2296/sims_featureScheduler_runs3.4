1808a8d1e1d7393eed7c20b124e9f9b802407ad4
Peter Yoachim <yoachim@uw.edu>
2024-01-31 09:15:28 -0800

    restore

---
+++ baseline/restore.py
@@ -0,0 +1,32 @@
+import numpy as np
+from rubin_scheduler.scheduler.utils import restore_scheduler
+from baseline import example_scheduler, sched_argparser
+from rubin_scheduler.scheduler.model_observatory import ModelObservatory
+
+
+if __name__ == "__main__":
+
+    parser = sched_argparser()
+    args = parser.parse_args()
+    args.setup_only = True
+
+    scheduler = example_scheduler(args)
+
+    observation_id = 1359637-3
+
+    #observation_id = 5000
+
+    observatory = ModelObservatory()
+
+    scheduler, observatory = restore_scheduler(observation_id, scheduler,
+                                               observatory, 'baseline_v3.4_10yrs.db', filter_sched=None, fast=True)
+
+    conditions = observatory.return_conditions()
+    conditions.mounted_filters = ['u', 'g', 'r', 'i', 'z']
+
+    for sl in scheduler.survey_lists:
+        for survey in sl:
+            print(survey, survey.calc_reward_function(conditions))
+
+    obs = np.concatenate(scheduler.survey_lists[2][4].generate_observations(conditions))
+    import pdb ; pdb.set_trace()
