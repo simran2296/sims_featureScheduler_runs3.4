fba5eddc63559c5f382856f00b80585e4d3bd6a0
Peter Yoachim <yoachim@uw.edu>
2024-02-19 14:57:35 -0800

    typo

--- too_elab_heavy/gen_too_elab.py
+++ too_elab_heavy/gen_too_elab.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     for day in [2, 3]:
         for filts in ["gz", "griz", "ugrizy"]:
             for rate in [32, 64, 128]:
-                print("python too_elab.py --too_days %i --too_rate %i --too_filters %s" % (day, rate, filts))
+                print("python too_elab_heavy.py --too_days %i --too_rate %i --too_filters %s" % (day, rate, filts))
