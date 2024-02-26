ec1a31dee88a1fe91ac7f3d342a20fea5799987d
Peter Yoachim <yoachim@uw.edu>
2024-02-26 09:12:00 -0800

    extra_ss

---
+++ extra_ss/README.md
@@ -0,0 +1,1 @@
+Run the near-sun survey slightly more often to simulate what it would be like if we had lots of inner solar system ToO events.
---
+++ extra_ss/extra_ss.sh
@@ -0,0 +1,5 @@
+python extra_ss.py --neo_night_pattern 2
+python extra_ss.py --neo_night_pattern 3
+python extra_ss.py --neo_night_pattern 4
+python extra_ss.py --neo_night_pattern 5
+python extra_ss.py --neo_night_pattern 7
