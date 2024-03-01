00759cd34c6f6074a09aece78330329e104180cc
Peter Yoachim <yoachim@uw.edu>
2024-02-29 16:16:53 -0800

    typo

--- ddf_acor/gen_ddf_acor.py
+++ ddf_acor/gen_ddf_acor.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
 
     # Should be similar to baseline
-    print("python ddf_presched.py --ddf_season_frac 0.2 --low_season_fraction 0 --low_season_rate 0")
+    print("python ddf_acor.py --ddf_season_frac 0.2 --low_season_fraction 0 --low_season_rate 0")
 
     ddf_seasons = [0.1, 0.15, 0.2, 0.25, 0.3]
 
@@ -13,4 +13,4 @@ if __name__ == "__main__":
         for low_seaons_fraction in low_seaons_fractions:
             if low_seaons_fraction > ddf_season:
                 for low_season_rate in low_season_rates:
-                    print("python ddf_presched.py --ddf_season_frac %.2f --low_season_fraction %.2f --low_season_rate %.2f" % (ddf_season, low_seaons_fraction, low_season_rate))
+                    print("python ddf_acor.py --ddf_season_frac %.2f --low_season_fraction %.2f --low_season_rate %.2f" % (ddf_season, low_seaons_fraction, low_season_rate))
