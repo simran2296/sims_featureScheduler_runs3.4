146e36ff3b9c8825e966b0826fa1c959557fa37c
Peter Yoachim <yoachim@uw.edu>
2024-03-01 12:48:38 -0800

    roll pause 2

---
+++ roll_pause2/README.md
@@ -0,0 +1,1 @@
+Like roll_pause, but inserting a second constant season so years 4 and 7 should be constant.
---
+++ roll_pause2/roll_pause2.sh
@@ -0,0 +1,6 @@
+python roll_pause2.py --mjd_plus 0
+python roll_pause2.py --mjd_plus 60
+python roll_pause2.py --mjd_plus 120
+python roll_pause2.py --mjd_plus 180
+python roll_pause2.py --mjd_plus 270
+python roll_pause2.py --mjd_plus 365
