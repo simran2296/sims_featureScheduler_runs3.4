20f666be026d3e5b21c908df859441ae3504aa78
beckermr <becker.mr@gmail.com>
2024-03-12 14:36:05 -0500

    ENH try another uniform rolling scheme

---
+++ roll_uniform_early_half/roll_uniform_early_half.sh
@@ -0,0 +1,4 @@
+python roll_uniform_early_half.py --mjd_plus 0
+python roll_uniform_early_half.py --mjd_plus 67
+python roll_uniform_early_half.py --mjd_plus 180
+python roll_uniform_early_half.py --mjd_plus 253
