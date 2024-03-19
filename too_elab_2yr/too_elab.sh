863feac200989e6772adc208f40ff9a1dc100b63
Peter Yoachim <yoachim@uw.edu>
2024-03-19 13:19:47 -0700

    sh

---
+++ too_elab_2yr/too_elab.sh
@@ -0,0 +1,8 @@
+python too_elab_2yr.py --too_days 2 --too_rate 32 --too_filters ugrizy
+python too_elab_2yr.py --too_days 3 --too_rate 32 --too_filters ugrizy
+python too_elab_2yr.py --too_days 2 --too_rate 64 --too_filters ugrizy
+python too_elab_2yr.py --too_days 3 --too_rate 64 --too_filters ugrizy
+python too_elab_2yr.py --too_days 2 --too_rate 128 --too_filters ugrizy
+python too_elab_2yr.py --too_days 3 --too_rate 128 --too_filters ugrizy
+python too_elab_2yr.py --too_days 2 --too_rate 128 --too_filters griz
+python too_elab_2yr.py --too_days 3 --too_rate 128 --too_filters griz
