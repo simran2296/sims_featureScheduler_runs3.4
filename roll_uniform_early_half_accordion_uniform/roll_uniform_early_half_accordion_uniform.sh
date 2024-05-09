fa670d70d17942c4c9f47e05d3fb4a1a239d74f6
beckermr <becker.mr@gmail.com>
2024-05-09 09:47:37 -0500

    ENH add accordion only during uniform years

---
+++ roll_uniform_early_half_accordion_uniform/roll_uniform_early_half_accordion_uniform.sh
@@ -0,0 +1,6 @@
+python roll_uniform_eh_au.py --mjd_plus 0 --acc_strength 1
+python roll_uniform_eh_au.py --mjd_plus 0 --acc_strength 0
+python roll_uniform_eh_au.py --mjd_plus 0 --acc_strength 0.1
+python roll_uniform_eh_au.py --mjd_plus 0 --acc_strength 0.25
+python roll_uniform_eh_au.py --mjd_plus 0 --acc_strength 0.5
+python roll_uniform_eh_au.py --mjd_plus 0 --acc_strength 0.75
