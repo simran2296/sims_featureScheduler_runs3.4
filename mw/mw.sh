ba5fa0fae92c55760a9efdf992b55d494c0c5e73
Peter Yoachim <yoachim@uw.edu>
2024-06-13 09:46:26 -0700

    comment out

--- mw/mw.sh
+++ mw/mw.sh
@@ -1,4 +1,4 @@
-python no_magell_roll.py
-python lmc_filter_balance.py
-python lmc_filter_balance_nr.py
+# python no_magell_roll.py
+# python lmc_filter_balance.py
+# python lmc_filter_balance_nr.py
 python lmc_filter_balance_mid.py
